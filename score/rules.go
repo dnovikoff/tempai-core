4c316ca232c0033f4fd21ef0f40d70962d3f4f17
Dmitry Novikov <novikov.dmitri.a@gmail.com>
2018-01-18 13:43:48 +0300

    Cleanup and vendor update

--- score/rules.go
+++ score/rules.go
@@ -3,14 +3,11 @@ package score
 type Money int
 
 type Rules struct {
-	StartMoney    Money   `json:"start-money"`
-	Uma           []Money `json:"uma"`
-	Oka           Money   `json:"oka"`
-	ManganRound   bool    `json:"mangan-round"`
-	KazoeYakuman  bool    `json:"kazoe-yakuman"`
-	DoubleYakuman bool    `json:"double-yakuman"`
-	YakumanSum    bool    `json:"yakuman-sum"`
-	HonbaValue    Money   `json:"honba-value"`
+	ManganRound   bool  `json:"mangan-round"`
+	KazoeYakuman  bool  `json:"kazoe-yakuman"`
+	DoubleYakuman bool  `json:"double-yakuman"`
+	YakumanSum    bool  `json:"yakuman-sum"`
+	HonbaValue    Money `json:"honba-value"`
 }
 
 func (this Rules) GetHonbaMoney(honba Honba) Money {
