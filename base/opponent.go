cbe4f52c37e08881776ffab1f5a1488666aaf8c8
Dmitry Novikov <novikov.dmitri.a@gmail.com>
2018-02-04 16:30:24 +0300

    Opponent advance and test

--- base/opponent.go
+++ base/opponent.go
@@ -8,4 +8,20 @@ const (
 	Right
 	Front
 	Left
+	OpponentCount
 )
+
+func (o Opponent) Advance(x int) Opponent {
+	if x < 0 {
+		x += (((-x) / 4) + 1) * 4
+	}
+	return (o + Opponent(x)) % OpponentCount
+}
+
+func (o Opponent) Next() Opponent {
+	return o.Advance(int(Right))
+}
+
+func (o Opponent) Prev() Opponent {
+	return o.Advance(int(Left))
+}
