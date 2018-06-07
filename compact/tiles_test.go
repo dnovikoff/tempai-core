5b9a82f68d903c07ac153181c45d9ca2e4105cfe
Dmitry Novikov <novikov.dmitri.a@gmail.com>
2018-06-07 19:07:25 +0300

    More tests

--- compact/tiles_test.go
+++ compact/tiles_test.go
@@ -39,3 +39,62 @@ func TestCTS(t *testing.T) {
 	assert.Equal(t, "12m", x.Tiles().String())
 	assert.Equal(t, 2, x.Count())
 }
+
+func TestTilesNormalize(t *testing.T) {
+	assert.Equal(t, FromTile(tile.Man1), FromTile(tile.Man1).Normalize())
+	assert.Equal(t, FromTile(tile.Red), FromTile(tile.Red).Normalize())
+	assert.EqualValues(t, 0, FromTile(tile.TileEnd).Normalize())
+
+	assert.Equal(t, AllTiles, AllTiles.Normalize())
+	assert.Equal(t, KokushiTiles, KokushiTiles.Normalize())
+}
+
+func TestTilesInvert(t *testing.T) {
+	assert.EqualValues(t, 0, AllTiles.Invert())
+	assert.Equal(t, FromTile(tile.Pin4), AllTiles.Unset(tile.Pin4).Invert())
+}
+
+func TestTilesMerge(t *testing.T) {
+	x1 := FromTiles(tile.Man1, tile.Red, tile.Pin1)
+	x2 := FromTiles(tile.Man2, tile.Red, tile.Pin2)
+	assert.Equal(t, FromTiles(tile.Man1, tile.Man2, tile.Red, tile.Pin1, tile.Pin2), x1.Merge(x2))
+
+	assert.Equal(t, FromTiles(tile.Man1, tile.Pin1), x1.Sub(x2))
+	assert.Equal(t, FromTiles(tile.Man2, tile.Pin2), x2.Sub(x1))
+}
+
+func TestTilesEach(t *testing.T) {
+	x1 := FromTiles(tile.Man1, tile.Red, tile.Pin1)
+	t.Run("all", func(t *testing.T) {
+		var res tile.Tiles
+		x1.Each(func(t tile.Tile) bool {
+			res = append(res, t)
+			return true
+		})
+		assert.Equal(t, tile.Tiles{tile.Man1, tile.Pin1, tile.Red}, res)
+	})
+	t.Run("sequence range", func(t *testing.T) {
+		var res tile.Tiles
+		x1.EachRange(tile.SequenceBegin, tile.SequenceEnd, func(t tile.Tile) bool {
+			res = append(res, t)
+			return true
+		})
+		assert.Equal(t, tile.Tiles{tile.Man1, tile.Pin1}, res)
+	})
+	t.Run("not sequence range", func(t *testing.T) {
+		var res tile.Tiles
+		x1.EachRange(tile.SequenceEnd, tile.TileEnd, func(t tile.Tile) bool {
+			res = append(res, t)
+			return true
+		})
+		assert.Equal(t, tile.Tiles{tile.Red}, res)
+	})
+	t.Run("stop", func(t *testing.T) {
+		var res tile.Tiles
+		x1.Each(func(t tile.Tile) bool {
+			res = append(res, t)
+			return false
+		})
+		assert.Equal(t, tile.Tiles{tile.Man1}, res)
+	})
+}
